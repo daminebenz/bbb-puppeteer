3eb4017c3e6dadd090642494f375744e7738df9f
Mohamed Amine Ben Salah <contact@damine.wtf>
2019-10-16 17:47:35 -0300

    updating test-01.sh script file

--- test-01.sh
+++ test-01.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+#
+# Script Name: test-01.sh
+#
+# Author: Mohamed Amine Ben Salah
+# Date : 16/10/2019
+#
+# Description: The following script runs a BBB Test starting from tests/poll.test.js file
+#              and it logs all the Test Log into logs/test-01
+#
+# Error Log: Any errors or output associated with the script can be found in logs/test-01
+#
+
+
+
 # deleting log file content at the start of the test
 echo -n "" > logs/test-01
 
