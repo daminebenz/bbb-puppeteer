bae5aa69b5c1ae42cdaf1a878123c4d110041504
Mohamed Amine Ben Salah <contact@damine.wtf>
2019-10-21 15:21:44 -0300

    autotest01 ready

--- autotest01/app.js
+++ autotest01/app.js
@@ -1,21 +1,5 @@
-const puppeteer = require('puppeteer');
-const testingMic = require('../tests/audio/mic.test');
-const testingAudio = require('../tests/audio/listen.test');
+const multiViewers = require('../tests/page/Puppeteer1.test');
+const findViewer = require('../tests/page/Puppeteer2.test');
 
-puppeteer.launch({
-    headless: false,
-    args: [ '--use-fake-ui-for-media-stream',
-            '--window-size=800,600']
-    }).then(async browser => {
-    browser.newPage().then(async () => {
-    try {
-        testingMic;
-        testingAudio;
-        process.exit[0]
-    }
-    catch (error) {
-        console.log({error});
-        process.exit[1]
-    }
-    browser.close();
-})})
+multiViewers;
+findViewer;
