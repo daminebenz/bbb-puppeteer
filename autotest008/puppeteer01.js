e3a2ed4e2e05a431cef2826ce2ca1d53187a2406
Mohamed Amine Ben Salah <contact@damine.wtf>
2019-11-28 13:15:01 -0300

    .

--- autotest008/puppeteer01.js
+++ autotest008/puppeteer01.js
@@ -4,10 +4,12 @@ const basePath = process.argv[3]
 var path = require('path'); 
 const TIMELIMIT_SECONDS = parseInt(process.argv[4])
 const TIMELIMIT_MILLISECONDS = TIMELIMIT_SECONDS * 1000;
-const metrics = {}
+const metric = {}
+const fpsObj = {}
 const moment = require('moment');
 
 var metricsJSON = path.join(__dirname,`./${basePath}/metrics.json`)
+var fpsJSON = path.join(__dirname,`./${basePath}/metricsFPS.json`)
 var fs = require("fs");
 
 async function puppeteer1() {
@@ -24,72 +26,48 @@ async function puppeteer1() {
         await page.waitForSelector('[aria-describedby^="modalDismissDescription"]', {timeout: 0});
         await page.click('[aria-describedby^="modalDismissDescription"]');
         for (var i = TIMELIMIT_MILLISECONDS; i >= 0; i--) {
-            // rAF
-            await page.evaluate(()=>{
-                let x = window.requestAnimationFrame = function() {
-                    return window.requestAnimationFrame ||
-                        window.webkitRequestAnimationFrame ||
-                        window.mozRequestAnimationFrame ||
-                        window.msRequestAnimationFrame ||
-                        window.oRequestAnimationFrame ||
-                        function(f) {
-                            window.setTimeout(f,1e3/60);
+            await page.evaluate(async () => {
+                return new Promise((resolve) => {
+                    window.requestAnimationFrame(resolve);                
+                    var fps = 60;
+                    var now;
+                    var then = Date.now();
+                    var interval = 1000/fps;
+                    var delta;
+                    var counter = 0;
+                    var first = then;
+                    function draw() {
+                        requestAnimationFrame(draw);
+                        now = Date.now();
+                        delta = now - then;
+                        if (delta > interval) {
+                            then = now - (delta % interval);
+                            var time_el = (then - first)/1000;
+                            let fpsVal = ++counter + 'f / ' + parseInt(time_el) + 's = ' + parseInt(counter/time_el) + 'fps'
+                            console.log(fpsVal);
                         }
-                }(); 
-                return x
+                    }
+                    draw();
+                })
             })
+            const date = new Date()
+            const metrics = await page.metrics();
+            const performance = await page.evaluate(() => performance.toJSON())
 
-            var $ = await page.evaluate(()=>document.querySelector.bind(document));
+            metric['dateObj'] = moment(date).format('DD/MM/YYYY hh:mm:ss');
+            metric['metricsObj'] = metrics;
+            metric['performancesObj'] = performance;
 
-
-            var fps = 60;
-            var now;
-            var then = Date.now();
-            var interval = 1000/fps;
-            var delta;
-
-            // For this demo
-            var counter!
-            var first = then;
-
-            var second_since = Date.now();
-            var second = 0;
-            var second_fps = 0;
-            
-            function draw() {
-                
-                // Calculating REAL FPS
-                if (second > 1000) {
-                    second_since = Date.now();
-                    second = 0;                    
-                    second_fps = 0;
-                }
-                else {
-                    second = Date.now() - second_since;
-                    ++second_fps;
-                }
-                
-        
-                
-                now = Date.now();
-                delta = now - then;
-                
-                if (delta > interval) {
-
-                    then = now - (delta % interval);
-                    var time_el = (then - first)/1000;
-                    return time_el
-                }
-                console.log(++counter + 'f / ' + parseInt(time_el) + 's = ' + parseInt(counter/time_el) + 'fps')
-
-            }
-
-            draw();
-        }
-        await page.waitFor(60000)
-
-        process.exit(0)
-    }   
+            fs.appendFileSync(metricsJSON, JSON.stringify(metric)+'\n', (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                };
+            });
+            await page.waitFor(60000)
+            process.exit(0)
+        }  
+    } 
     catch(error){
         console.log({error})
         process.exit(1);
